826de9fef3b5089e17db668099e3fc50c8e0aab9
unknown <nchowdary62@gmail.com>
2020-10-21 20:03:13 +0530

    commited successfully

---
+++ src/main/java/enums/Browsers.java
@@ -0,0 +1,11 @@
+package enums;
+
+public enum Browsers {
+		
+		CHROME,
+		FIREFOX,
+		IE
+
+	}
+
+
---
+++ src/main/java/testbase/Wait.java
@@ -0,0 +1,24 @@
+package testbase;
+
+public class Wait extends TestBase{
+	
+private static String explicitWait;
+	
+	private static String implicitWait;
+	
+	private static String pageloadTime;
+	
+	
+	public static String getExplicitWait() {
+		return Repository.getProperty("explicitWait");
+	}
+
+	public static String getImplicitWait() {
+		return Repository.getProperty("implicitWait");
+	}
+
+	public static String getPageloadTime() {
+		return Repository.getProperty("pageloadTime");
+	}
+
+}
